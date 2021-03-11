9669b94c676a12825790b2fa1ec77efd249c2678
sallesgabriel <emilio.salles.gabriel@gmail.com>
2021-03-11 00:58:49 -0300

    Criada app base

--- pypro/urls.py
+++ pypro/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from pypro.base import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home)
 ]
-
