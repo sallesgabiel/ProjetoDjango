a4c8f3f2b3ee2fbf69ec4889cafcaf25a15fa15a
sallesgabriel <emilio.salles.gabriel@gmail.com>
2021-03-13 01:10:09 -0300

    criado env sample

--- pypro/settings.py
+++ pypro/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '+mb6z0e)d4pdl_%z3guoj(ab=buiijvy!de+-bux0d-umq3xof'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
